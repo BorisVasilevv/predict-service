23c8da0011f98f0ab3d072e9c2dc4cb3c30a55a9
BorisVasilev <BacR009@yandex.ru>
2024-06-14 16:51:00 +0500

    small change

--- models/examination.py
+++ models/examination.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, Date
 from sqlalchemy.orm import relationship
 
 from .base import Base
@@ -8,7 +8,7 @@ class Examination(Base):
     __tablename__ = 'examination'
     id = Column(Integer, primary_key=True)
     examination_type_id = Column(Integer, ForeignKey('examination_type.id'))
-    datetime = Column(DateTime)
+    datetime = Column(Date)
     examination_type = relationship("Examination_Type", back_populates="examinations")
     schedule_record = relationship("ScheduleRecord", back_populates="examination", uselist=False)
 
